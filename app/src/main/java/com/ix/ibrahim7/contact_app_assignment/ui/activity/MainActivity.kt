6fdf11b8194a47ff08f1d3c96526e12da590234e
ibrahim mushtaha <ibrahim.mushtaha2@gmail.com>
2021-03-30 23:25:48 +0300

    update main activity

--- app/src/main/java/com/ix/ibrahim7/contact_app_assignment/ui/activity/MainActivity.kt
+++ app/src/main/java/com/ix/ibrahim7/contact_app_assignment/ui/activity/MainActivity.kt
@@ -2,13 +2,18 @@ package com.ix.ibrahim7.contact_app_assignment.ui.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
 import com.google.firebase.analytics.FirebaseAnalytics
+import com.ix.ibrahim7.contact_app_assignment.R
 import com.ix.ibrahim7.contact_app_assignment.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var mBinding: ActivityMainBinding
-
+    private var navHostFragment: Fragment? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,6 +22,14 @@ class MainActivity : AppCompatActivity() {
 
         setSupportActionBar(mBinding.toolbar)
         FirebaseAnalytics.getInstance(this)
+        navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.fragment)
+
+        val navController = navHostFragment!!.findNavController()
+        val appBarConfiguration = AppBarConfiguration.Builder(navController.graph).build()
+        NavigationUI.setupWithNavController(
+            mBinding.toolbar, navController, appBarConfiguration
+        )
 
 
     }
