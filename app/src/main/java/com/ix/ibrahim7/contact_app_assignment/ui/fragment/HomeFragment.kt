a28ccdeed3c6bc683ac7defb8d167ef450a31c94
ibrahim mushtaha <ibrahim.mushtaha2@gmail.com>
2021-03-30 21:47:53 +0300

    Add Category layout

---
+++ app/src/main/java/com/ix/ibrahim7/contact_app_assignment/ui/fragment/HomeFragment.kt
@@ -0,0 +1,47 @@
+package com.ix.ibrahim7.contact_app_assignment.ui.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.ix.ibrahim7.contact_app_assignment.BR
+import com.ix.ibrahim7.contact_app_assignment.R
+import com.ix.ibrahim7.contact_app_assignment.adapter.GenericAdapter
+import com.ix.ibrahim7.contact_app_assignment.databinding.FragmentHomeBinding
+import com.ix.ibrahim7.contact_app_assignment.databinding.FragmentListUserBinding
+import com.ix.ibrahim7.contact_app_assignment.model.Category
+
+
+class HomeFragment : Fragment(),GenericAdapter.OnListItemViewClickListener<Category> {
+
+    private lateinit var mBinding: FragmentHomeBinding
+
+    private val userAdapter by lazy {
+        GenericAdapter(R.layout.item_category, BR.user,this)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        mBinding = FragmentHomeBinding.inflate(layoutInflater).apply {
+            executePendingBindings()
+        }
+        return mBinding.root
+    }
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+
+
+
+
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onClickItem(itemViewModel: Category, type: Int) {
+
+    }
+
+}
